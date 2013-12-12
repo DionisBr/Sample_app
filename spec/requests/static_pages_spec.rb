24925d1fc731b2dccdccc9c1a52bf6247c0d42a6
DionisBr <dionisxx7@gmail.com>
2013-12-12 21:16:24 +0200

    Ch 4 finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,16 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+	it "should  have the base title " do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "SampleApp | Home")
+                        :text => "SampleApp")
+    end
+	
+    it "should not have the custon title " do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                        :text => ' | Home')
     end
   end
 
@@ -23,10 +29,16 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should  have the base title " do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "SampleApp | Help")
+                        :text => "SampleApp")
+    end
+	
+    it "should not have the custon title " do
+      visit '/static_pages/help'
+      page.should_not have_selector('title',
+                        :text => ' | Help')
     end
   end
 
@@ -37,10 +49,16 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
-    it "should have the title 'About Us'" do
+    it "should  have the base title " do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "SampleApp | About Us")
+                        :text => "SampleApp")
+    end
+	
+    it "should not have the custon title " do
+      visit '/static_pages/about'
+      page.should_not have_selector('title',
+                        :text => ' | About Us')
     end
   end
 	
@@ -51,10 +69,16 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Contact')
     end
 
-    it "should have the title 'Contact'" do
+    it "should  have the base title " do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                    :text => "SampleApp | Contact")
+                        :text => "SampleApp")
+    end
+	
+    it "should not have the custon title " do
+      visit '/static_pages/contact'
+      page.should_not have_selector('title',
+                        :text => ' | Contact')
     end
   end
 end
